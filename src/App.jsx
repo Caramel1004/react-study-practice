757d48b2b3d66528c0832d51c9877b277dde4baa
Caramel1004 <Caramel1004.js@gmail.com>
2024-03-01 18:26:25 +0900

    🎨 Core concepts 설명 박스 컴포넌트 생성 및  props 추가
    Core concepts 설명 박스 컴포넌트 생성 및 props 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+import logo from './images/logo.svg';
 import './App.css';
 
 const getRandomInteger = count => {
-  return Math.floor(Math.random() * (count + 1));
-} 
+  return Math.floor(Math.random() * count);
+}
 
 const Header = () => {
   const headerDescription = ['리액트 스터디', '리액트 연습', '리린이 등장'];
@@ -13,26 +13,47 @@ const Header = () => {
 
   return (
     <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {description}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        {description}
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
   );
+}
+
+const CoreConcept = props => {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
 }
 
 function App() {
   return (
     <div className="App">
-      <Header className="App-header"></Header>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="핵심 UI 빌딩블록"
+              image={logo} />
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
