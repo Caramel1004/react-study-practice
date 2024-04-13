71fdcaa3550139d5d8014c25483a65d27c7a5e88
Caramel1004 <Caramel1004.js@gmail.com>
2024-04-14 04:06:26 +0900

    🎨 [art] 컴포넌트 단위로 코드 파일 분류 후 APP.jsx 파일에 import
    🎨 [art] 컴포넌트 단위로 코드 파일 분류 후 APP.jsx 파일에 import

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,7 @@
-import logo from './images/logo.svg';
 import './App.css';
-
-const getRandomInteger = count => {
-  return Math.floor(Math.random() * count);
-}
-
-const Header = () => {
-  const headerDescription = ['리액트 스터디', '리액트 연습', '리린이 등장'];
-  const descriptionCount = headerDescription.length;
-
-  const description = headerDescription[getRandomInteger(descriptionCount)];
-
-  return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        {description}
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  );
-}
-
-const CoreConcept = props => {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  )
-}
+import logo from './images/logo.svg';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
 function App() {
   return (
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import logo from '../images/logo.svg';
+
+const getRandomInteger = count => {
+    return Math.floor(Math.random() * count);
+  }
+  
+export default function Header(){
+    const headerDescription = ['리액트 스터디', '리액트 연습', '리린이 등장'];
+    const descriptionCount = headerDescription.length;
+  
+    const description = headerDescription[getRandomInteger(descriptionCount)];
+  
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {description}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    );
+  }
